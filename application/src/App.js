b8d2045b0f81dab224320fda9072cf68c058b36d
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-05-16 20:54:32 +0200

    SecondComponent | Added

--- application/src/App.js
+++ application/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LandingComponent from './components/LandingComponent'
+import LandingComponent from './components/LandingComponent';
+import SecondComponent from './components/SecondComponent';
 import NavBar from './components/NavBar';
 // import QuizSlider from "./components/QuizSlider";
 
@@ -9,6 +10,7 @@ function App() {
 		<>
 			<NavBar />
 			<LandingComponent />
+			<SecondComponent />
 			{/* <QuizSlider /> */}
 		</>
 	);
