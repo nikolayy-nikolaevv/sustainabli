4cfc619e37476269f738dc1cb907a1d3d2b36205
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-05-22 10:20:21 +0200

    NavBar | Battery Animation Refactor

--- application/src/components/NavBar.jsx
+++ application/src/components/NavBar.jsx
@@ -1,44 +1,47 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/images/logo.png';
-
+import { useRef, useEffect} from 'react';
 
 export default function NavBar() {
-    window.addEventListener("scroll",function(event){
-        const batteryLevel = document.getElementById('batteryLevel');
-        const scrollPos = window.scrollY;
-        const batteryProgress = 10 + scrollPos / 100;
-        batteryLevel.style.width = batteryProgress + "%";
-        const colorStart = "rgb(" + (237 - batteryProgress * 1.5) + ","+(118 + batteryProgress * 0.4)+",80)";
-        const colorEnd = "rgb(" + (242 - batteryProgress * 1.5) + ","+(179 + batteryProgress * 0.4)+",74)";
-        batteryLevel.style.backgroundImage = "linear-gradient(to right, " + colorStart + ", " + colorEnd + ")";
+	const batteryLevel = useRef();
 
-    });
-    return (
-            <Navbar className="NavBar" fixed="top">
-                <Navbar.Brand href="/" className="NavBarLogo">
-                    <img
-                        src={Logo}
-                        className="NavBarLogoPic d-inline-block align-top"
-                        alt="Li Logo"
-                    />
-                </Navbar.Brand>
-                <Nav className="NavBarLink">
-                    <Nav.Link className="NavBarLinkText" href="#lithium-superhero">Lithium - a Superhero</Nav.Link>
-                    <Nav.Link className="NavBarLinkText" href="#dark-side-lithium">The Dark Side of Lithium</Nav.Link>
-                    <Nav.Link className="NavBarLinkText" href="#what-you-can-do">What You Can Do</Nav.Link>
-                    <Nav.Link className="NavBarLinkText" href="#participate">Participate</Nav.Link>
-                </Nav>
-                <div className="batteryContainer">
+	useEffect(() => {
+		window.addEventListener("scroll", function (event) {
+			const scrollPos = window.scrollY;
+			const batteryProgress = 10 + scrollPos / 100;
+			batteryLevel.current.style.width = batteryProgress + "%";
+			const colorStart = "rgb(" + (237 - batteryProgress * 1.5) + "," + (118 + batteryProgress * 0.4) + ",80)";
+			const colorEnd = "rgb(" + (242 - batteryProgress * 1.5) + "," + (179 + batteryProgress * 0.4) + ",74)";
+			batteryLevel.current.style.backgroundImage = "linear-gradient(to right, " + colorStart + ", " + colorEnd + ")";
+		});
+	}, [])
 
-                    <div className="batteryOuter">
-                        <div id="batteryLevel"></div>
-                    </div>
-                    <div className="batteryBump"></div>
-                </div>
+	return (
+		<Navbar className="NavBar" fixed="top">
+			<Navbar.Brand href="/" className="NavBarLogo">
+				<img
+					src={Logo}
+					className="NavBarLogoPic d-inline-block align-top"
+					alt="Li Logo"
+				/>
+			</Navbar.Brand>
+			<Nav className="NavBarLink">
+				<Nav.Link className="NavBarLinkText" href="#lithium-superhero">Lithium - a Superhero</Nav.Link>
+				<Nav.Link className="NavBarLinkText" href="#dark-side-lithium">The Dark Side of Lithium</Nav.Link>
+				<Nav.Link className="NavBarLinkText" href="#what-you-can-do">What You Can Do</Nav.Link>
+				<Nav.Link className="NavBarLinkText" href="#participate">Participate</Nav.Link>
+			</Nav>
+			<div className="batteryContainer">
 
-            </Navbar>
+				<div className="batteryOuter">
+					<div id="batteryLevel" ref={batteryLevel}></div>
+				</div>
+				<div className="batteryBump"></div>
+			</div>
 
-    );
+		</Navbar>
+
+	);
 }
 
