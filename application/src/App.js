0dbcf3a00b4f47043d7fbefb13b27800c9ddb9a7
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-06 22:20:06 +0200

    Metadata | Changed content; App | Removed unused imports

--- application/src/App.js
+++ application/src/App.js
@@ -6,9 +6,7 @@ import SuperheroComponent from './components/SuperheroComponent';
 import AntiheroComponent from './components/AntiheroComponent';
 import NavBar from './components/NavBar';
 // import QuizSlider from "./components/QuizSlider";
-import TextBox from './components/TextBox';
 import Battery from './components/Battery';
-import TextImage from './components/TextImage';
 
 function App() {
 	return (
