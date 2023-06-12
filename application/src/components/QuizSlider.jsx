b9c5b28bfc31a71fc34f6b10afac51b82773ccb7
cortese <lino.cortese@student.reutlingen-university.de>
2023-06-12 22:21:15 +0200

    QuizSlider | Added remaining questions

--- application/src/components/QuizSlider.jsx
+++ application/src/components/QuizSlider.jsx
@@ -23,17 +23,47 @@ export default function QuizSlider() {
         <div id="participate" className='QuizSliderContainer' >
             <Carousel activeIndex={index} className='QuizSlider' interval={null} slide={false}>
                 <Carousel.Item>
-                    <h3>First slide label</h3>
+                    <h3>Which of the following technology products have you purchased that use lithium batteries? (Select all that apply)</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
                 </Carousel.Item>
                 <Carousel.Item>
-                    <h3>Second slide label</h3>
+                    <h3>How long does the lithium battery typically last in the products you have purchased?</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
                 </Carousel.Item>
                 <Carousel.Item>
-                    <h3>Third slide label</h3>
+                    <h3>Have you ever experienced safety issues with the use of lithium batteries in the products you have purchased?</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h3>If you answered yes to the previous question, what safety issues have you encountered? (Select all that apply)</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h3>How do you dispose of used or defective lithium batteries from the products you have purchased?</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h3>How important is the battery life of a technology product to you when making a purchase?</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h3>How important is the safety of the battery in a technology product to you when making a purchase?</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h3>How do you see the use of lithium batteries in technology evolving in the future?</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h3>How old are you?</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
                 </Carousel.Item>
