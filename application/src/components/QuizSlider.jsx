28edcfc53137917b7e75ce85f1da567f1a6a9fe3
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-12 22:07:19 +0200

    QuizSlider | Added Dynamic Controls

--- application/src/components/QuizSlider.jsx
+++ application/src/components/QuizSlider.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 export default function QuizSlider() {
     const [index, setIndex] = useState(0);
+    const [slideCount, setSlideCount] = useState(null);
 
     const incrementSlide = (selectedIndex, e) => {
         setIndex(index + 1);
@@ -12,23 +13,29 @@ export default function QuizSlider() {
         setIndex(index - 1);
     };
 
+    useEffect(() => {
+        const slides = document.getElementsByClassName("carousel-item");
+        setSlideCount(slides.length - 1 );
+    }, []);
+
+
     return (
-        <div id="participate" className='QuizSliderContainer'>
-            <Carousel activeIndex={index} className='QuizSlider' interval={null}>
+        <div id="participate" className='QuizSliderContainer' >
+            <Carousel activeIndex={index} className='QuizSlider' interval={null} slide={false}>
                 <Carousel.Item>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} />
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
                 </Carousel.Item>
                 <Carousel.Item>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} />
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
                 </Carousel.Item>
                 <Carousel.Item>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} />
+                    <Controls incrementSlide={incrementSlide} decrementSlide={decrementSlide} index={index} slideCount={slideCount} />
                 </Carousel.Item>
             </Carousel>
         </div>
@@ -36,11 +43,25 @@ export default function QuizSlider() {
     );
 }
 
-function Controls({ incrementSlide, decrementSlide }) {
+function Controls({ incrementSlide, decrementSlide, index, slideCount, handleSubmit }) {
+    if (index === 0) {
+        return (
+            <>
+                <button className="CarouselNextButton CarouselControl" onClick={incrementSlide}>Next</button>
+            </>
+        );
+    }
+
+    if (index === slideCount) {
+        return (
+            <button className="CarouselNextButton CarouselControl" onClick={handleSubmit}>Submit</button>
+        );
+    }
+
     return (
         <>
-            <button className="CarouselPrevButton" onClick={decrementSlide}>Prev</button>
-            <button className="CarouselNextButton" onClick={incrementSlide}>Next</button>
+            <button className="CarouselNextButton CarouselControl" onClick={incrementSlide}>Next</button>
+            <button className="CarouselPrevButton CarouselControl" onClick={decrementSlide}>Prev</button>
         </>
     );
 }
