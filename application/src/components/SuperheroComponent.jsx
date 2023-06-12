acbe91d92f563055f3968e49972da57d4d4b8888
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-12 16:15:17 +0200

    CSS | TextBoxes have same height

--- application/src/components/SuperheroComponent.jsx
+++ application/src/components/SuperheroComponent.jsx
@@ -2,7 +2,7 @@ import TextBox from "./TextBox";
 
 export default function SuperheroComponent() {
 	return (
-		<TextBox primaryColor="#F2B34A" secondaryColor="#6153BE" titleColor="#1E1E1E" title="Lithium - a Superhero" contentColor="#1E1E1E"
+		<TextBox primaryColor="#F2B34A" secondaryColor="#6153BE" titleColor="#1E1E1E" title="Lithium - a real Superhero" contentColor="#1E1E1E"
 			boxType="RightBox" />
 	);
 
