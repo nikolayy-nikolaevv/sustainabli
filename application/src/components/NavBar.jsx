1d4e6f95cff13ad5197f99719c25b5d523fc0bbe
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-12 22:25:48 +0200

    NavBar | Battery Gets Full when end of Site

--- application/src/components/NavBar.jsx
+++ application/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar({logo}) {
 	useEffect(() => {
 		window.addEventListener("scroll", function (event) {
 			const scrollPos = window.scrollY;
-			const batteryProgress = 10 + scrollPos / 100;
+			const batteryProgress = 10 + scrollPos / 70;
 			batteryLevel.current.style.width = batteryProgress + "%";
 			const colorStart = "rgb(" + (237 - batteryProgress * 1.5) + "," + (118 + batteryProgress * 0.4) + ",80)";
 			const colorEnd = "rgb(" + (242 - batteryProgress * 1.5) + "," + (179 + batteryProgress * 0.4) + ",74)";
