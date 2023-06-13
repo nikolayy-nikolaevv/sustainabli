26f69ddc9d1c299dcdb50558722d789e351161d1
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-13 17:52:50 +0200

    Bubble | Fixed Warnings

--- application/src/components/Bubble.jsx
+++ application/src/components/Bubble.jsx
@@ -3,19 +3,19 @@ export default function Bubble({bubbleId, icon, bubbleCounter, onClickInput, set
     const myId = "Bubble" + {bubbleCounter};
   return(
     <>
-      {bubbleId == bubbleCounter ? (
+      {bubbleId === bubbleCounter ? (
         <div className = "Bubble" id = {myId} style = {{pointerEvents: 'auto', backgroundColor: `${color}`, height: `${size}`, 
           width: `${size}`, top: `${top}`, left: `${left}`}} 
           onClick={() => {onClickInput.fire();
             setCounter(counter + 1);
             setText(texts[counter]);
             document.getElementById(myId).style.visibility = "hidden"}}>
-          <img src = {icon} style = {{height: `${size}`}}></img>
+          <img alt="" src = {icon} style = {{height: `${size}`}}></img>
         </div>
         ) : (
         <div className = "Bubble" style = {{pointerEvents: 'none', backgroundColor: `${color}`, height: `${size}`, 
         width: `${size}`, top: `${top}`, left: `${left}`}}>
-          <img src = {icon} style = {{height: `${size}`}}></img>
+          <img alt="" src = {icon} style = {{height: `${size}`}}></img>
         </div>
         )
       }
