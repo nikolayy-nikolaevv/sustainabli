a82fba348a92db37e4b937a6eae1e0417564805d
Your Name <magdalena.meier@student.reutlingen-university.de>
2023-06-14 21:22:43 +0200

    BatteryAnimation | Fast click bug fix

--- application/src/components/Bubble.jsx
+++ application/src/components/Bubble.jsx
@@ -7,8 +7,9 @@ export default function Bubble({bubbleId, icon, bubbleCounter, onClickInput, set
         <div className = "Bubble" id = {myId} style = {{pointerEvents: 'auto', backgroundColor: `${color}`, height: `${size}`, 
           width: `${size}`, top: `${top}`, left: `${left}`}} 
           onClick={() => {onClickInput.fire();
-            setCounter(counter + 1);
             setText(texts[counter]);
+            setCounter(counter + 1);
+            setTimeout(() => onClickInput.fire(), 1500);
             document.getElementById(myId).style.visibility = "hidden"}}>
           <img alt="" src = {icon} style = {{height: `${size}`}}></img>
         </div>
