9df2da568161129e8adb3340fbf27441c76dc514
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-15 11:00:09 +0200

    Amogu | CSS Position Fix Test

--- application/src/components/LandingHeader.jsx
+++ application/src/components/LandingHeader.jsx
@@ -3,10 +3,10 @@ import Amogu from "../assets/images/amogu.svg";
 export default function LandingHeader({ incrementLogo, decrementLogo, logo }) {
 	return (
 		<div className="LandingHeader">
+			<div className="Amogu">
+				<img src={Amogu} className="AmoguPic" alt="Amogu" />
+			</div>
 			<div className="LandingHeaderTitle">
-				<div className="Amogu">
-					<img src={Amogu} className="AmoguPic" alt="Amogu" />
-				</div>
 				<div className="LandingHeaderTitleText" itemProp='InnerShadowText'>Sustainab</div>
 				<div className="LandingHeaderLogo" onClick={incrementLogo} onContextMenu={decrementLogo}>
 					<img className="LandingHeaderLogoPic" src={logo} alt="Periodic Table Li Logo" />
