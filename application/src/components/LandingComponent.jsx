47db96f5ed35186111112f83fc1e477bdbc3955f
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-15 09:56:49 +0200

    LandingHeader | Added amogu

--- application/src/components/LandingComponent.jsx
+++ application/src/components/LandingComponent.jsx
@@ -4,21 +4,21 @@ import AtomMiddle from "../assets/images/atom_middle.png";
 import AtomBottom from "../assets/images/atom_bottom.png";
 
 export default function LandingComponent({ incrementLogo, decrementLogo, logo }) {
-    return (
-        <>
-            <div id="landing" className="LandingComponent Anchor">
-                <div className="AtomTop AtomSpinner">
-                    <img src={AtomTop} className="AtomImage" alt="Spinning Atom" />
-                </div>
-                <div className="AtomMiddle AtomSpinner">
-                    <img src={AtomMiddle} className="AtomImage" alt="Spinning Atom" />
-                </div>
-                <div className="AtomBottom AtomSpinner">
-                    <img src={AtomBottom} className="AtomImage" alt="Spinning Atom" />
-                </div>
-                <LandingHeader incrementLogo={incrementLogo} logo={logo} decrementLogo={decrementLogo} />
-            </div>
+	return (
+		<>
+			<div id="landing" className="LandingComponent Anchor">
+				<div className="AtomTop AtomSpinner">
+					<img src={AtomTop} className="AtomImage" alt="Spinning Atom" />
+				</div>
+				<div className="AtomMiddle AtomSpinner">
+					<img src={AtomMiddle} className="AtomImage" alt="Spinning Atom" />
+				</div>
+				<div className="AtomBottom AtomSpinner">
+					<img src={AtomBottom} className="AtomImage" alt="Spinning Atom" />
+				</div>
+				<LandingHeader incrementLogo={incrementLogo} logo={logo} decrementLogo={decrementLogo} />
+			</div>
 
-        </>
-    );
+		</>
+	);
 }
--- application/src/components/LandingHeader.jsx
+++ application/src/components/LandingHeader.jsx
@@ -1,15 +1,20 @@
+import Amogu from "../assets/images/amogu.svg";
+
 export default function LandingHeader({ incrementLogo, decrementLogo, logo }) {
-    return (
-        <div className="LandingHeader">
-            <div className="LandingHeaderTitle">
-                <div className="LandingHeaderTitleText" itemProp='InnerShadowText'>Sustainab</div>
-                <div className="LandingHeaderLogo" onClick={incrementLogo} onContextMenu={decrementLogo}>
-                    <img className="LandingHeaderLogoPic" src={logo} alt="Periodic Table Li Logo" />
-                </div>
-            </div>
-            <div className="LandingHeaderSubTitle">
-                <div className="LandingHeaderSubTitleText">Lithium can be pretty sus, learn more!</div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="LandingHeader">
+			<div className="LandingHeaderTitle">
+				<div className="Amogu">
+					<img src={Amogu} className="AmoguPic" alt="Amogu" />
+				</div>
+				<div className="LandingHeaderTitleText" itemProp='InnerShadowText'>Sustainab</div>
+				<div className="LandingHeaderLogo" onClick={incrementLogo} onContextMenu={decrementLogo}>
+					<img className="LandingHeaderLogoPic" src={logo} alt="Periodic Table Li Logo" />
+				</div>
+			</div>
+			<div className="LandingHeaderSubTitle">
+				<div className="LandingHeaderSubTitleText">Lithium can be pretty sus, learn more!</div>
+			</div>
+		</div>
+	);
 }
