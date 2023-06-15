f6df28312c80eaaca8eb597146d138a4ca3588f7
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2023-06-15 11:39:04 +0200

    Bubbles | Added Animation and Background

--- application/src/components/Battery.jsx
+++ application/src/components/Battery.jsx
@@ -8,62 +8,62 @@ import sunGlasses from "../assets/images/Sun_glasses.svg";
 import oldBattery from "../assets/images/Old_battery.svg";
 
 export default function Battery() {
-  const text1 = 'Can you help your battery recover? Try lowering the temperature!'
-  const text2 = "Well done! Extreme temperatures can damage the battery and shorten its lifespan. " + 
-    "Now, get rid of the band-aid." 
-  const text3 = "Good job! Shocks and impacts can damage your battery, so don't drop it! " + 
-    "Poor little guy looks tired. Time for an energy boost!";
-  const text4 = "Ahh, much better! Keep the battery between 20% and 80% and avoid fully discharging or charging it. If " +
-    "possible, use original chargers. Now, let's relax a little.";
-  const text5 = "Nice! Want your battery to stay relaxed? Use power-saving modes to reduce energy consumption. " +
-    "But no matter what you do, at some point your battery needs to retire...";
-  const text6 = "Fantastic! Old batteries like to retire to electronics retailers or to official collection points " +
-    "so that precious metals can be recycled. Now you know how to treat your battery right. Well done!"
-    
-  const texts = [text1, text2, text3, text4, text5, text6];
-  const [counter, setCounter] = useState(1);
-  const [text, setText] = useState(texts[0])
+	const text1 = 'Can you help your battery recover? Try lowering the temperature!'
+	const text2 = "Well done! Extreme temperatures can damage the battery and shorten its lifespan. " +
+		"Now, get rid of the band-aid."
+	const text3 = "Good job! Shocks and impacts can damage your battery, so don't drop it! " +
+		"Poor little guy looks tired. Time for an energy boost!";
+	const text4 = "Ahh, much better! Keep the battery between 20% and 80% and avoid fully discharging or charging it. If " +
+		"possible, use original chargers. Now, let's relax a little.";
+	const text5 = "Nice! Want your battery to stay relaxed? Use power-saving modes to reduce energy consumption. " +
+		"But no matter what you do, at some point your battery needs to retire...";
+	const text6 = "Fantastic! Old batteries like to retire to electronics retailers or to official collection points " +
+		"so that precious metals can be recycled. Now you know how to treat your battery right. Well done!"
 
-  const STATE_MACHINE = "State Machine";
-  const INPUT_NAME = "Switch";
+	const texts = [text1, text2, text3, text4, text5, text6];
+	const [counter, setCounter] = useState(1);
+	const [text, setText] = useState(texts[0])
 
-  const { rive, RiveComponent } = useRive({
-    src: "battery.riv",
-    stateMachines: STATE_MACHINE,
-    autoplay: true
-  });
+	const STATE_MACHINE = "State Machine";
+	const INPUT_NAME = "Switch";
 
-  const onClickInput = useStateMachineInput(
-    rive,
-    STATE_MACHINE,
-    INPUT_NAME
-  );
+	const { rive, RiveComponent } = useRive({
+		src: "battery.riv",
+		stateMachines: STATE_MACHINE,
+		autoplay: true
+	});
 
-  return (
-      <div id="what-you-can-do">
-    <div className = "AnimationContainer">
-    <div className="BatteryAnimation Anchor">
-      <RiveComponent style={{ height: "50vw" }} />
-      <div className = "TextBubble">
-        <p>{text}</p>
-      </div>
-      <Bubble bubbleId = "1" icon = {thermometer} bubbleCounter = {counter} onClickInput = {onClickInput} 
-        setCounter = {setCounter} setText = {setText} counter = {counter} texts = {texts} color = "#2E2B42"
-        size = "15vw" top = "30%" left = "20%" />  
-      <Bubble bubbleId = "2" icon = {bandAid} bubbleCounter = {counter} onClickInput = {onClickInput} 
-        setCounter = {setCounter} setText = {setText} counter = {counter} texts = {texts} color = "#2E2B42"
-        size = "20vh" top = "60%" left = "10%" />  
-      <Bubble bubbleId = "3" icon = {lightning} bubbleCounter = {counter} onClickInput = {onClickInput} 
-        setCounter = {setCounter} setText = {setText} counter = {counter} texts = {texts} color = "#2E2B42"
-        size = "15vh" top = "20%" left = "90%" />  
-      <Bubble bubbleId = "4" icon = {sunGlasses} bubbleCounter = {counter} onClickInput = {onClickInput} 
-        setCounter = {setCounter} setText = {setText} counter = {counter} texts = {texts} color = "#2E2B42"
-        size = "15vh" top = "80%" left = "80%" />  
-      <Bubble bubbleId = "5" icon = {oldBattery} bubbleCounter = {counter} onClickInput = {onClickInput} 
-        setCounter = {setCounter} setText = {setText} counter = {counter} texts = {texts} color = "#2E2B42"
-        size = "18vh" top = "10vh" left = "10vh" />
-    </div>
-    </div>
-      </div>
-  );
+	const onClickInput = useStateMachineInput(
+		rive,
+		STATE_MACHINE,
+		INPUT_NAME
+	);
+
+	return (
+		<div id="what-you-can-do">
+			<div className="AnimationContainer">
+				<div className="BatteryAnimation Anchor">
+					<RiveComponent style={{ height: "50vw" }} />
+					<div className="TextBubble">
+						<p>{text}</p>
+					</div>
+					<Bubble bubbleId="1" icon={thermometer} bubbleCounter={counter} onClickInput={onClickInput}
+						setCounter={setCounter} setText={setText} counter={counter} texts={texts} color="#2E2B42"
+						size="15vw" top="30%" left="20%" />
+					<Bubble bubbleId="2" icon={bandAid} bubbleCounter={counter} onClickInput={onClickInput}
+						setCounter={setCounter} setText={setText} counter={counter} texts={texts} color="#2E2B42"
+						size="20vh" top="60%" left="10%" />
+					<Bubble bubbleId="3" icon={lightning} bubbleCounter={counter} onClickInput={onClickInput}
+						setCounter={setCounter} setText={setText} counter={counter} texts={texts} color="#2E2B42"
+						size="15vh" top="20%" left="90%" />
+					<Bubble bubbleId="4" icon={sunGlasses} bubbleCounter={counter} onClickInput={onClickInput}
+						setCounter={setCounter} setText={setText} counter={counter} texts={texts} color="#2E2B42"
+						size="15vh" top="80%" left="80%" />
+					<Bubble bubbleId="5" icon={oldBattery} bubbleCounter={counter} onClickInput={onClickInput}
+						setCounter={setCounter} setText={setText} counter={counter} texts={texts} color="#2E2B42"
+						size="18vh" top="10vh" left="10vh" />
+				</div>
+			</div>
+		</div>
+	);
 }
--- application/src/components/Bubble.jsx
+++ application/src/components/Bubble.jsx
@@ -1,25 +1,31 @@
-export default function Bubble({bubbleId, icon, bubbleCounter, onClickInput, setText, setCounter, counter, texts,
-  color, size, top, left}) {
-    const myId = "Bubble" + {bubbleCounter};
-  return(
-    <>
-      {Number(bubbleId) === Number(bubbleCounter) ? (
-        <div className = "Bubble" id = {myId} style = {{pointerEvents: 'auto', backgroundColor: `${color}`, height: `${size}`, 
-          width: `${size}`, top: `${top}`, left: `${left}`}} 
-          onClick={() => {onClickInput.fire();
-            setText(texts[counter]);
-            setCounter(counter + 1);
-            setTimeout(() => onClickInput.fire(), 1500);
-            document.getElementById(myId).style.visibility = "hidden"}}>
-          <img alt="" src = {icon} style = {{height: `${size}`}}></img>
-        </div>
-        ) : (
-        <div className = "Bubble" style = {{pointerEvents: 'none', backgroundColor: `${color}`, height: `${size}`, 
-        width: `${size}`, top: `${top}`, left: `${left}`}}>
-          <img alt="" src = {icon} style = {{height: `${size}`}}></img>
-        </div>
-        )
-      }
-    </> 
-  )
+export default function Bubble({ bubbleId, icon, bubbleCounter, onClickInput, setText, setCounter, counter, texts,
+	color, size, top, left }) {
+	const myId = "Bubble" + { bubbleCounter };
+	return (
+		<>
+			{Number(bubbleId) === Number(bubbleCounter) ? (
+				<div className="Bubble BubbleActive" id={myId} style={{
+					pointerEvents: 'auto', backgroundColor: `${color}`, height: `${size}`,
+					width: `${size}`, top: `${top}`, left: `${left}`
+				}}
+					onClick={() => {
+						onClickInput.fire();
+						setText(texts[counter]);
+						setCounter(counter + 1);
+						setTimeout(() => onClickInput.fire(), 1500);
+						document.getElementById(myId).style.visibility = "hidden"
+					}}>
+					<img alt="" src={icon} style={{ height: `${size}` }}></img>
+				</div>
+			) : (
+				<div className="Bubble" style={{
+					pointerEvents: 'none', backgroundColor: `${color}`, height: `${size}`,
+					width: `${size}`, top: `${top}`, left: `${left}`
+				}}>
+					<img alt="" src={icon} style={{ height: `${size}` }}></img>
+				</div>
+			)
+			}
+		</>
+	)
 }
